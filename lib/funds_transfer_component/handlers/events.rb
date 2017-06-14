2958dedb6aa186de42c87ab8a47d9371c5779fdc
Scott Bellware <sbellware@gmail.com>
2017-06-14 17:03:29 -0500

    [STEP] Deposit to-do

--- lib/funds_transfer_component/handlers/events.rb
+++ lib/funds_transfer_component/handlers/events.rb
@@ -28,6 +28,10 @@ module FundsTransferComponent
           previous_message: initiated
         )
       end
+
+      handle Withdrawn do |withdrawn|
+        # TODO Send Deposit command to the Account component using account client
+      end
     end
   end
 end
